a6875de18efdb2ab565ebc9e0ab0ce1694e9aa70
Ali <alen_zx@yahoo.com>
2021-12-05 13:02:49 +0300

    Added Migration Database

--- SolarCoffee.Web/Program.cs
+++ SolarCoffee.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SolarDbContext>(options => {
+    options.EnableDetailedErrors();
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Solar.dev"));
+});
 
 var app = builder.Build();
 
