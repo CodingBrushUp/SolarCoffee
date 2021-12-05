ad6df1326bbe6bd00bca1041407606ba680c08d3
Ali <alen_zx@yahoo.com>
2021-12-05 17:16:33 +0300

    Added Product Inventory Models

--- SolarCoffee.Data/Models/Customer.cs
+++ SolarCoffee.Data/Models/Customer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +12,6 @@ namespace SolarCoffee.Data.Models
         public string? LastName { get; set;} = string.Empty;
 
         public CustomerAddress? Address { get; set; }
+        public string Description { get; set; }
     }
 }
